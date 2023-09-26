12d037a769fe90138d7448a5cdbf49a5dc67618f
Md. Hasibuzzaman <45512066+hasib2019@users.noreply.github.com>
2023-09-26 16:25:21 +0600

    Update ApiList.js

--- url/ApiList.js
+++ url/ApiList.js
@@ -1,5 +1,5 @@
-const liveIp = 'http://localhost:8000/api/';
-// const liveIp = 'https://www.gold.crystalgolds.com/api/';
+// const liveIp = 'http://localhost:8000/api/';
+const liveIp = 'https://www.gold.crystalgolds.com/api/';
 
 export const loginApi = liveIp + 'login';
 export const registerApi = liveIp + 'users';
