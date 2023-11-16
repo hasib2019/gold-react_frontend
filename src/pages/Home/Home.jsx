b9e9fa50977175bb89ff8296878ea5356dcf4146
Md Saifur Rahman <saifur1985bd@gmail.com>
2023-11-16 10:16:42 +0600

    Update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from "react";
-import HeaderLogo from "@/assets/images/Crystal.jpg";
-import { Link } from "react-router-dom";
 import "@/assets/style.css";
-import phonePng from "@/assets/images/phone.png";
-import whatsappPng from "@/assets/images/whatsapp.png";
 import axios from "axios";
 import { liveRateApi } from "../../../url/ApiList";
+
 const Home = () => {
   const [liveRateData, setLiveRateData] = useState([]);
   const [previousLiveRateData, setPreviousLiveRateData] = useState([]);
-  console.log({ getRate: liveRateData });
 
   const firstDataGet = async () => {
     try {
@@ -26,118 +22,130 @@ const Home = () => {
       setPreviousLiveRateData(updatedData);
       setLiveRateData(updatedData);
     } catch (error) {
-      // Handle error if needed
       console.error("Error fetching live rates:", error);
     }
   };
 
-  const getLiveRates = async (previousData) => {
+  let previousData = [];
+
+  const getLiveRates = async () => {
     try {
       const getRate = await axios.get(liveRateApi);
       const getRateData = getRate?.data;
-      const updatedData = getRateData.map((item, index) => {
-        const previousDataItem = previousData[index];
-        console.log({ previousDataItem, item });
-        const updatedItem = {
-          ...item,
-          ask_buy_color:
-          item?.ask_buy === previousDataItem?.ask_buy? "black": item?.ask_buy < previousDataItem?.ask_buy? "red": "green",
-            // item?.ask_buy == previousDataItem?.ask_buy ? "black" : item?.ask_buy < previousDataItem?.ask_buy ? "green" : item?.ask_buy > previousDataItem?.ask_buy ? "red" : "black",
-          bid_sell_color:
-          item?.bid_sell === previousDataItem?.bid_sell? "black": item?.bid_sell < previousDataItem?.bid_sell? "red": "green",
-            // item?.bid_sell > previousDataItem?.bid_sell ? "green" : "red",
-        };
-        return updatedItem;
-      });
-      setLiveRateData(updatedData);
+      if (previousData.length === getRateData.length) {
+        const updatedData = getRateData.map((item, index) => {
+          const previousDataItem = previousData[index];
+          const updatedItem = {
+            ...item,
+            ask_buy_color:
+              item?.ask_buy === previousDataItem?.ask_buy ? "black" : item?.ask_buy < previousDataItem?.ask_buy ? "red" : "green",
+            bid_sell_color:
+              item?.bid_sell === previousDataItem?.bid_sell ? "black" : item?.bid_sell < previousDataItem?.bid_sell ? "red" : "green",
+          };
+          return updatedItem;
+        });
+        setLiveRateData(updatedData);
+      } else {
+        console.log("Previous data doesn't match the length of new data.");
+      }
+      previousData = getRateData;
     } catch (error) {
-      // Handle error if needed
       console.error("Error fetching live rates:", error);
     }
   };
 
   useEffect(() => {
-    // Initial call
     firstDataGet();
     const intervalId = setInterval(() => {
-      // getLiveRates(previousLiveRateData);
       getLiveRates(liveRateData);
     }, 2000);
     return () => clearInterval(intervalId);
   }, []);
 
   return (
-      <div style={{ paddingBottom: 30 }}>
-        <table className="table-1">
-          <tr>
-            <th>PRODUCT</th>
-            <th>$ BID(SELL)</th>
-            <th>$ ASK(BUY)</th>
-          </tr>
-          <tr>
-            <th rowspan="2">{liveRateData[0]?.type}</th>
-            <td className={`medium ${liveRateData[0]?.bid_sell_color}`}>
-              {liveRateData[0]?.bid_sell}
-            </td>
-            <td className={`medium ${liveRateData[0]?.ask_buy_color}`}>
-              {liveRateData[0]?.ask_buy}
-            </td>
-          </tr>
-          <tr className="font-14">
-            <td>
-              <span className="red">Low :</span> {liveRateData[0]?.low}
-            </td>
-            <td>
-              <span className="green">High :</span> {liveRateData[0]?.high}
-            </td>
-          </tr>
-        </table>
-        <table>
-          {liveRateData?.map((item, index) => {
-            return (
-              item.type !== "GOLD OZ" &&
-              item.type !== "GOLD" && (
-                <tr key={index}>
-                  <th>{item.type}</th>
-                  {/* <td className={`w-half ${item?.ask_buy_color}`}>
-                    {item?.ask_buy}
-                  </td>
-                  <td className={`w-half ade ${item?.bid_sell_color}`}>
-                    {item?.bid_sell}
-                  </td> */}
-                  <td className={`w-half`}>
-                    {item.type == "TEN TOLA BAR"
-                      ? "TTB"
-                      : item.type == "KILO BAR 995"
-                      ? "1KG"
-                      : item.type == "KILO BAR 9999"
+    <div style={{ paddingBottom: 30 }}>
+      <table className="table-1">
+        <tr>
+          <th>PRODUCT</th>
+          <th>$ BID(SELL)</th>
+          <th>$ ASK(BUY)</th>
+        </tr>
+        <tr>
+          <th rowspan="2">{liveRateData[0]?.type}</th>
+          <td className={`medium ${liveRateData[0]?.bid_sell_color}`}>
+            {liveRateData[0]?.bid_sell}
+          </td>
+          <td className={`medium ${liveRateData[0]?.ask_buy_color}`}>
+            {liveRateData[0]?.ask_buy}
+          </td>
+        </tr>
+        <tr className="font-14">
+          <td>
+            <span className="red">Low :</span> {liveRateData[0]?.low}
+          </td>
+          <td>
+            <span className="green">High :</span> {liveRateData[0]?.high}
+          </td>
+        </tr>
+      </table>
+      <table>
+
+        <tr>
+          <th>{liveRateData[4]?.type}</th>
+          <td className={`w-half`}>TTB</td>
+          <td className={`w-half ade`}>AED</td>
+          <td>
+            <div>
+              <span className={`medium ${liveRateData[4]?.ask_buy_color}`}>
+                {liveRateData[4]?.ask_buy}
+              </span>
+              <div className="font-12 range">
+                <div>
+                  <span className="red">L : </span> {liveRateData[4]?.low}
+                </div>
+                <div>
+                  <span className="green">H : </span> {liveRateData[4]?.high}
+                </div>
+              </div>
+            </div>
+          </td>
+        </tr>
+        {liveRateData?.map((item, index) => {
+          return (
+            item.type !== "GOLD OZ" &&
+            item.type !== "GOLD" &&
+            item.type !== "TEN TOLA BAR" && (
+              <tr key={index}>
+                <th>{item.type}</th>
+                <td className={`w-half`}>
+                  {item.type == "KILO BAR 995"
+                    ? "1KG"
+                    : item.type == "KILO BAR 9999"
                       ? "1KG"
                       : "1GM"}
-                  </td>
-                  <td className={`w-half ade`}>AED</td>
-                  <td>
-                    <div>
-                      <span className={`medium ${item?.ask_buy_color}`}>
-                        {item?.ask_buy}
-                      </span>
-                      <div className="font-12 range">
-                        <div>
-                          <span className="red">L : </span> {item?.low}
-                        </div>
-                        <div>
-                          <span className="green">H : </span> {item?.high}
-                        </div>
+                </td>
+                <td className={`w-half ade`}>AED</td>
+                <td>
+                  <div>
+                    <span className={`medium ${item?.ask_buy_color}`}>
+                      {item?.ask_buy}
+                    </span>
+                    <div className="font-12 range">
+                      <div>
+                        <span className="red">L : </span> {item?.low}
+                      </div>
+                      <div>
+                        <span className="green">H : </span> {item?.high}
                       </div>
                     </div>
-                  </td>
-                </tr>
-              )
-            );
-          })}
-        </table>
-      </div>
-
-   
+                  </div>
+                </td>
+              </tr>
+            )
+          );
+        })}
+      </table>
+    </div>
   );
 };
 
